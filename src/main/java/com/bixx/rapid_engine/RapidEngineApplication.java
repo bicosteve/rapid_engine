850aee8b8c183a753e90c7f9edcd2df94496cb83
bsteve <bico.steve@betika.com>
2026-03-07 12:14:25 +0300

    added app packages

--- src/main/java/com/bixx/rapid_engine/RapidEngineApplication.java
+++ src/main/java/com/bixx/rapid_engine/RapidEngineApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RapidEngineApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RapidEngineApplication.class, args);
 	}
