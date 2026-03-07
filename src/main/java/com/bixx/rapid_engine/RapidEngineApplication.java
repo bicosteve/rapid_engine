f578f4e69d5c3b59aa234c56888d83733c0855f1
bsteve <bico.steve@betika.com>
2026-03-07 11:32:43 +0300

    proejct set up

---
+++ src/main/java/com/bixx/rapid_engine/RapidEngineApplication.java
@@ -0,0 +1,13 @@
+package com.bixx.rapid_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RapidEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RapidEngineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bixx/rapid_engine/RapidEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bixx.rapid_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RapidEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
