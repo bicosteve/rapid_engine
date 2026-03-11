10c76bc8d5aa1fad86449a322296311e83e3b624
bsteve <bico.steve@betika.com>
2026-03-11 05:02:56 +0300

    added configuration properties on Main class

--- src/main/java/com/bixx/rapid_engine/RapidEngineApplication.java
+++ src/main/java/com/bixx/rapid_engine/RapidEngineApplication.java
@@ -2,10 +2,14 @@ package com.bixx.rapid_engine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableScheduling // This will enable scheduling for the producer
+@EnableTransactionManagement // Activates transaction management
+@ConfigurationPropertiesScan // Scans for classes annotated with @ConfigurationProperties(prefix="app.rabbitmq")
 public class RapidEngineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RapidEngineApplication.class, args);
