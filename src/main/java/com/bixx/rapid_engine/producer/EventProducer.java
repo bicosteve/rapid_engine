644e9a94dd2ef24441bc3d40a843ceab5f7c1cb3
bsteve <bico.steve@betika.com>
2026-05-03 13:35:10 +0300

    added scheduler time to avoid rate limiting on rundown api

--- src/main/java/com/bixx/rapid_engine/producer/EventProducer.java
+++ src/main/java/com/bixx/rapid_engine/producer/EventProducer.java
@@ -1,29 +1,22 @@
 package com.bixx.rapid_engine.producer;
 
-import java.time.Duration;
-import java.time.LocalDate;
-import static java.time.LocalDate.now;
-import java.util.List;
-
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-
 import com.bixx.rapid_engine.config.RundownConfig;
 import com.bixx.rapid_engine.models.Event;
 import com.bixx.rapid_engine.models.Meta;
 import com.bixx.rapid_engine.models.RundownResponse;
 import com.bixx.rapid_engine.rabbitmq.RabbitMQConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
-
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
@@ -37,41 +30,46 @@ public class EventProducer {
     private final ObjectMapper objectMapper;
     private final RedisTemplate<String, String> stringRedisTemplate;
 
-    public void fetchEvents() {
-        List<Integer> sportIds = this.rundownConfig.getSportsId();
-        sportIds.forEach(sportId -> {
-            try {
-                this.fetchAndPublishEvents(sportId);
-                Thread.sleep(3000);
-                // wait for 3s before next request.
-                // necessary to avoid 429 rate limit exception.
-            } catch (InterruptedException ex) {
-                Thread.currentThread().interrupt();
-                log.error("Producer::sleep interrupted for sport {}", sportId);
-            } catch (Exception e) {
-                log.error("Producer::error fetching sport {} with error {}",
-                        sportId,
-                        e.getMessage());
-            }
-        });
+    public void fetchEvents(Integer sportsId){
+        try {
+            this.fetchAndPublishEvents(sportsId);
+        } catch(Exception e) {
+            log.error("Producer::Error fetching sport {} with error {}", sportsId, e.getMessage());
+        }
+//        List<Integer> sportIds = this.rundownConfig.getSportsId();
+//        sportIds.forEach(sportId -> {
+//            try {
+//                this.fetchAndPublishEvents(sportId);
+//                Thread.sleep(3000);
+//                // wait for 3s before next request.
+//                // necessary to avoid 429 rate limit exception.
+//            } catch(InterruptedException ex) {
+//                Thread.currentThread().interrupt();
+//                log.error("Producer::sleep interrupted for sport {}", sportId);
+//            } catch(Exception e) {
+//                log.error("Producer::error fetching sport {} with error {}",
+//                        sportId,
+//                        e.getMessage());
+//            }
+//        });
     }
 
-    private void fetchAndPublishEvents(Integer sportId) throws Exception {
+    private void fetchAndPublishEvents(Integer sportId) throws Exception{
 
         // 01. Check in Redis the last existing deltaLastId
         String redisKey = "rundown:delta_last_id:%s".formatted(sportId);
         String deltaLastId = this.stringRedisTemplate.opsForValue().get(redisKey);
 
         // 02. Build the URL
-        LocalDate today = now();
+        LocalDate today = LocalDate.now();
         String url;
-        if (deltaLastId == null) {
+        if(deltaLastId == null) {
             log.info("Producer::sport {} - no delta found, fetching all events", sportId);
             url = "%s/sports/%s/events/%s?affiliate_ids=%s".formatted(
                     this.rundownConfig.getHost(),
                     sportId,
                     today,
-                    this.rundownConfig.getHost());
+                    this.rundownConfig.getAffiliateId());
         } else {
             log.info("Producer::sport {} - delta found {}, fetching updates", sportId, deltaLastId);
             url = "%s/sports/%s/events/%s?affiliate_ids=%s&delta_last_id=%s".formatted(
@@ -95,7 +93,7 @@ public class EventProducer {
                 entity,
                 String.class);
 
-        if (response.getStatusCode() != HttpStatus.OK) {
+        if(response.getStatusCode() != HttpStatus.OK) {
             log.error(
                     "Producer::sport {} - API returned {}",
                     sportId,
@@ -110,7 +108,7 @@ public class EventProducer {
 
         // 06. Check for Events
         List<Event> events = rundownResponse.getEvents();
-        if (events == null || events.isEmpty()) {
+        if(events == null || events.isEmpty()) {
             log.warn(
                     "Producer::sport {} - no events found for {}",
                     sportId,
@@ -121,7 +119,7 @@ public class EventProducer {
         // 07. Get events & meta from response
         Meta meta = rundownResponse.getMeta();
         String newDeltaLastId = meta.getDeltaLastId();
-        if (newDeltaLastId != null) {
+        if(newDeltaLastId != null) {
             this.stringRedisTemplate
                     .opsForValue()
                     .set(redisKey, newDeltaLastId, Duration.ofHours(24));
--- src/main/java/com/bixx/rapid_engine/utils/MatchSyncTask.java
+++ src/main/java/com/bixx/rapid_engine/utils/MatchSyncTask.java
@@ -1,22 +1,40 @@
 package com.bixx.rapid_engine.utils;
 
+import com.bixx.rapid_engine.config.RundownConfig;
 import com.bixx.rapid_engine.producer.EventProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 @RequiredArgsConstructor
 @Slf4j
 public class MatchSyncTask {
+    private final RundownConfig rundownConfig;
     private final EventProducer eventProducer;
+    private int sportIndex = 0;
 
-    @Scheduled(fixedRate = 3_600_000, initialDelay = 20_000)
+
+    @Scheduled(fixedRate = 300_000, initialDelay = 20_000)
     public void fetchMatches(){
         // Scheduled to run after 1hr
         // delay the first call after the app start with 20s
+
+        List<Integer> sportIds = rundownConfig.getSportsId();
+        if(sportIds == null || sportIds.isEmpty())
+            return;
+
+        // Get the current sport ID and increment the index value for the next time
+        Integer sportId = sportIds.get(this.sportIndex);
+        log.info("Scheduler::Cycling to sport id {} ", sportId);
+
         log.info("Scheduler::triggering match producer");
-        this.eventProducer.fetchEvents();
+        this.eventProducer.fetchEvents(sportId);
+
+        // Use Round-robin logic
+        sportIndex = (sportIndex + 1) % sportIds.size();
     }
 }
