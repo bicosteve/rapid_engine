0c7a170c60770b1e705d5c56cb0ef5fd3d14e418
bsteve <bico.steve@betika.com>
2026-05-14 07:10:03 +0300

    fix(matchsynchtask): remove data point tracker error which caused the app to crash

--- src/main/java/com/bixx/rapid_engine/utils/MatchSyncTask.java
+++ src/main/java/com/bixx/rapid_engine/utils/MatchSyncTask.java
@@ -8,8 +8,6 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
-import java.time.Duration;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Component
@@ -24,7 +22,7 @@ public class MatchSyncTask {
 
     @Scheduled(fixedRate = 300_000, initialDelay = 20_000)
     public void fetchMatches(){
-        // Scheduled to run after 10mins
+        // Scheduled to run after 5mins
         // delay the first call after the app start with 20s
 
         // 01. Check for the sportsId provided in the config
@@ -37,52 +35,12 @@ public class MatchSyncTask {
         Integer sportId = sportIds.get(this.sportIndex);
         log.info("Cycling to sport id {} ", sportId);
 
-        log.info("Triggering match producer");
+        // 03. Track published events
+        log.info("Trigger match producer");
         int publishedEvents = this.eventProducer.fetchEvents(sportId);
-
-        // 04. Track published events
         log.info("Published events {} ", publishedEvents);
 
-        // 05. Use Round-robin logic
+        // 04. Use Round-robin logic
         sportIndex = (sportIndex + 1) % sportIds.size();
-    }
-
-    private boolean isBudgetExhausted(){
-        String usageKey = this.getDailyUsageKey();
-        String usage = this.stringRedisTemplate.opsForValue().get(usageKey);
-
-        if(usage == null)
-            return false;
-
-        int used = Integer.parseInt(usage);
-        log.info("Daily data points used {}/{} ", used, DAILY_DATA_POINT_BUDGET);
-
-        return used >= DAILY_DATA_POINT_BUDGET;
-    }
-
-    private void trackDataPointUsed(int eventsPublished){
-        // Estimates the data points used
-        // each event has at most 3 markets,
-        int pointsUsed = eventsPublished * ESTIMATED_POINTS_PER_FETCH;
-
-        String usageKey = this.getDailyUsageKey();
-
-        // Increment counter - expire after 24hours
-        this.stringRedisTemplate.opsForValue().increment(usageKey, pointsUsed);
-        this.stringRedisTemplate.expire(usageKey, Duration.ofSeconds(24));
-
-        log.info(
-                "Estimated {} data points used for {} events ",
-                pointsUsed,
-                eventsPublished
-        );
-
-    }
-
-
-    private String getDailyUsageKey(){
-        // gets the dailyUsageKey
-        // key resets daily - includes today's date
-        return "rundown:data_points:%s".formatted(LocalDateTime.now());
     }
 }
